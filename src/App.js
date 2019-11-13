74797723c2b0580e342f81d565c4d2c4692ba2a0
Małgorzata Brożek <malgorzata.brozek@softwarehut.com>
2019-11-13 08:59:55 +0100

    starter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header.js";
+// import Slider from './components/Slider.js';
+// import Services from './components/Services';
+// import LeatestWork from './components/LatestWork';
+// import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello Kitthulu!
+      <Header />
+      {/* <Slider/>
+  <Services/>
+  <LeatestWork/>
+  <Footer/> */}
     </div>
   );
 }
