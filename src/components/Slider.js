0631f36ad5d364e3329d1d26f78df707e05acadc
Małgorzata Brożek <malgorzata.brozek@softwarehut.com>
2019-11-15 14:34:32 +0100

    Refactor: Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,27 +4,6 @@ import left from "../images/left.png";
 import right from "../images/right.png";
 
 const Slider = () => {
-  //     const images=[
-  //         "https://images.unsplash.com/photo-1511902467434-4677a533a674?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=210"
-  //         "https://images.unsplash.com/photo-1553755322-fb5d174a0656?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=210"
-  //         "https://images.unsplash.com/photo-1528301024193-8b8b6c7d9d6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=210"
-  //         "https://images.unsplash.com/photo-1438480478735-3234e63615bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=210"
-  //         "https://images.unsplash.com/photo-1541233349642-6e425fe6190e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=210"
-  //     "https://images.unsplash.com/photo-1515245696852-ae418cad9b62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=370&q=60"
-  //     ]
-  //     const [index, setIndex]= useState(0),
-
-  //     function goToPrevSlide() {
-  //         if(index===0){
-  //         return setIndex(index -1)}
-  //     }
-
-  // function goToNextSlide(){
-  //     if (index===images.length -1){
-  //         return setIndex(index=0)
-  //     }
-  // }
-
   return (
     <div className="slider">
       <div className="slider-rectangle">
